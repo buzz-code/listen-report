247d92531501ef1c0845f7ad690e589dbfc04dd8
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-03 19:38:00 +0300

    add utils folder to common

--- client/routers/RestrictRoute.js
+++ client/routers/RestrictRoute.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import { isAuthenticated } from '../utils/jwtUtil';
+import { isAuthenticated } from '../../common-modules/client/utils/jwtUtil';
 
 const RestrictRoute = ({ component: Component, ...rest }) => (
-    <Route
-        {...rest}
-        render={props =>
-            isAuthenticated() ? (
-                <Redirect
-                    to={{
-                        pathname: '/dashboard',
-                        state: { from: props.location },
-                    }}
-                />
-            ) : (
-                    <Component {...props} />
-            )
-        }
-    />
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Redirect
+          to={{
+            pathname: '/dashboard',
+            state: { from: props.location },
+          }}
+        />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
 );
 
 export default RestrictRoute;
