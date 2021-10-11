2a7293a110a59241f05328b0be5948acaf205a1f
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-10-11 22:14:21 +0300

    update query

--- server/utils/queryHelper.js
+++ server/utils/queryHelper.js
@@ -9,51 +9,51 @@ import ReportTeacher from "../models/reportTeacher.model";
 import ReportKindergarten from "../models/reportKindergarten.model";
 
 export function getUserByPhone(phone_number) {
-    return new User({ phone_number })
+    return new User().where({ phone_number })
         .fetch()
         .then(res => res.toJSON());
 }
 
 export function getStudentAndTeacherByUserIdAndPhone(user_id, phone_number) {
-    const student = new Student({ user_id, phone_number })
+    const student = new Student().where({ user_id, phone_number })
         .fetch({ require: false })
         .then(res => res ? res.toJSON() : null);
-    const teacher = new Teacher({ user_id, full_phone: phone_number })
+    const teacher = new Teacher().where({ user_id, full_phone: phone_number })
         .fetch({ require: false })
         .then(res => res ? res.toJSON() : null);
     return Promise.all([student, teacher]);
 }
 
 export function getKindergartenStudentByUserIdAndTz(user_id, tz) {
-    return new KindergartenStudent({ user_id, tz })
+    return new KindergartenStudent().where({ user_id, tz })
         .fetch({ require: false })
         .then(res => res ? res.toJSON() : null);
 }
 
 export function getTeacherByUserIdAndLastDigits(user_id, lastDigits) {
-    return new Teacher({ user_id })
+    return new Teacher().where({ user_id })
         .where('full_phone', 'like', '%' + lastDigits)
         .fetch({ require: false })
         .then(res => res ? res.toJSON() : null);
 }
 
 export function getReportTypeByUserId(userId) {
-    return new ReportType({ user_id: userId })
+    return new ReportType().where({ user_id: userId })
         .fetchAll()
         .then(res => res.toJSON());
 }
 
 export function getExistingStudentReport(student_id) {
-    return new Report({ student_id, report_date: moment().format('YYYY-MM-DD') })
+    return new Report().where({ student_id, report_date: moment().format('YYYY-MM-DD') })
         .fetchAll();
 }
 
 export function getExistingTeacherReport(teacher_id) {
-    return new ReportTeacher({ teacher_id, report_date: moment().format('YYYY-MM-DD') })
+    return new ReportTeacher().where({ teacher_id, report_date: moment().format('YYYY-MM-DD') })
         .fetchAll();
 }
 
 export function getExistingKindergartenStudentReport(student_id) {
-    return new ReportKindergarten({ student_id, report_date: moment().format('YYYY-MM-DD') })
+    return new ReportKindergarten().where({ student_id, report_date: moment().format('YYYY-MM-DD') })
         .fetchAll();
 }
