d3027e90f960a97861ffe77d9707518c0bf34280
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-11-12 10:33:58 +0200

    add teacher type table

--- client/containers/teachers/TeachersContainer.js
+++ client/containers/teachers/TeachersContainer.js
@@ -1,21 +1,41 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Table from '../../../common-modules/client/components/table/Table';
+import * as crudAction from '../../../common-modules/client/actions/crudAction';
+import { getPropsForAutoComplete } from '../../../common-modules/client/utils/formUtil';
 
-const getColumns = () => [
+const getColumns = ({ teacherTypes }) => [
   { field: 'tz', title: 'תעודת זהות' },
   { field: 'name', title: 'שם' },
   { field: 'full_phone', title: 'מספר טלפון' },
+  {
+    field: 'teacher_type_id',
+    title: 'סוג מורה',
+    columnOrder: 'teacher_types.name',
+    ...getPropsForAutoComplete('teacher_type_id', teacherTypes),
+  },
 ];
 const getFilters = () => [
   { field: 'tz', label: 'תעודת זהות', type: 'text', operator: 'like' },
   { field: 'name', label: 'שם', type: 'text', operator: 'like' },
   { field: 'full_phone', label: 'מספר טלפון', type: 'text', operator: 'like' },
+  { field: 'teacher_types.name', label: 'סוג מורה', type: 'text', operator: 'like' },
 ];
 
 const TeachersContainer = ({ entity, title }) => {
-  const columns = useMemo(() => getColumns(), []);
+  const dispatch = useDispatch();
+  const {
+    GET: { 'get-edit-data': editData },
+  } = useSelector((state) => state[entity]);
+
+  useEffect(() => {
+    dispatch(crudAction.customHttpRequest(entity, 'GET', 'get-edit-data'));
+  }, []);
+
+  const columns = useMemo(() => getColumns(editData || {}), [editData]);
   const filters = useMemo(() => getFilters(), []);
+
   const validateRow = useCallback((rowData) => {
     if (!rowData.tz) {
       return 'חובה להזין תעודת זהות';
--- server/controllers/teacher.controller.js
+++ server/controllers/teacher.controller.js
@@ -1,4 +1,41 @@
 import Teacher from '../models/teacher.model';
-import genericController from '../../common-modules/server/controllers/generic.controller';
+import TeacherType from '../models/teacherType.model';
+import { getListFromTable } from '../../common-modules/server/utils/common';
+import genericController, { applyFilters, fetchPage } from '../../common-modules/server/controllers/generic.controller';
 
-export const { findAll, findById, store, update, destroy, uploadMultiple } = genericController(Teacher);
+export const { findById, store, update, destroy, uploadMultiple } = genericController(Teacher);
+
+/**
+ * Find all the items
+ *
+ * @param {object} req
+ * @param {object} res
+ * @returns {*}
+ */
+export async function findAll(req, res) {
+    const dbQuery = new Teacher({ user_id: req.currentUser.id })
+        .query(qb => {
+            qb.leftJoin('teacher_types', 'teacher_types.id', 'teachers.teacher_type_id')
+            qb.select('teachers.*')
+        });
+    applyFilters(dbQuery, req.query.filters);
+    fetchPage({ dbQuery }, req.query, res);
+}
+
+
+/**
+ * Get edit data
+ *
+ * @param {object} req
+ * @param {object} res
+ * @returns {*}
+ */
+export async function getEditData(req, res) {
+    const [teacherTypes] = await Promise.all([
+        getListFromTable(TeacherType, req.currentUser.id),
+    ]);
+    res.json({
+        error: null,
+        data: { teacherTypes }
+    });
+}
